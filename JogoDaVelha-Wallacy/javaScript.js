7a4edbeace98ec5929cc5131c08c46e662d47f92
Wallacy <Wallacy.teixeira@outlook.com>
2023-09-18 19:47:53 -0300

    fff

--- JogoDaVelha-Wallacy/javaScript.js
+++ JogoDaVelha-Wallacy/javaScript.js
@@ -1,13 +1,24 @@
+// Seleciona todos os elementos HTML com o atributo 'data-cell' (as células do jogo)
 const cellElements = document.querySelectorAll("[data-cell]");
+
+// Seleciona o elemento do tabuleiro (board) por seu atributo 'data-board'
 const board = document.querySelector("[data-board]");
+
+// Seleciona o elemento de mensagem de vitória por seu atributo 'data-winning-message-text'
 const winningMenssageTextElement = document.querySelector(
   "[data-winning-menssage-text]"
 );
+
+// Seleciona o elemento de mensagem de vitória por seu atributo 'data-winning-message'
 const winningMenssage = document.querySelector("[data-winning-menssage]");
+
+// Seleciona o botão de reiniciar o jogo por seu atributo 'data-restart-button'
 const restartButton = document.querySelector("[data-restart-button]");
 
+// Variável para controlar de quem é a vez (X ou Círculo)
 let isCircleTurn;
 
+// Matriz que contém todas as combinações vencedoras possíveis no jogo
 const winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -19,9 +30,12 @@ const winningCombinations = [
   [2, 4, 6],
 ];
 
+// Função para iniciar o jogo
 const startGame = () => {
+  // Define que não é a vez do círculo
   isCircleTurn = false;
 
+  // Remove todas as classes 'circle' e 'x' das células, desvincula os eventos de clique e adiciona novamente o evento de clique com 'once: true' para que só possa ser clicado uma vez
   for (const cell of cellElements) {
     cell.classList.remove("circle");
     cell.classList.remove("x");
@@ -29,22 +43,29 @@ const startGame = () => {
     cell.addEventListener("click", handleClick, { once: true });
   }
 
+  // Define a classe CSS para indicar de quem é a vez (círculo ou x)
   setBoardHoverClass();
+
+  // Esconde a mensagem de vitória
   winningMenssage.classList.remove("show-winning-menssage");
 };
 
+// Função para encerrar o jogo (pode ser uma vitória ou empate)
 const endGame = (isDraw) => {
   if (isDraw) {
+    // Se for um empate, exibe a mensagem "Velha!"
     winningMenssageTextElement.innerText = "Velha!";
   } else {
+    // Se não for empate, exibe qual jogador venceu
     winningMenssageTextElement.innerText = isCircleTurn
       ? "O Venceu!"
       : "X Venceu!";
   }
-
+  // Exibe a mensagem de vitória
   winningMenssage.classList.add("show-winning-menssage");
 };
 
+// Função para verificar se algum jogador venceu
 const checkForWin = (currentPlayer) => {
   return winningCombinations.some((combination) => {
     return combination.every((index) => {
@@ -53,16 +74,19 @@ const checkForWin = (currentPlayer) => {
   });
 };
 
+// Função para verificar se o jogo empatou
 const checkForDraw = () => {
   return [...cellElements].every((cell) => {
     return cell.classList.contains("x") || cell.classList.contains("circle");
   });
 };
 
+// Função para marcar uma célula com o símbolo (X ou Círculo)
 const placeMark = (cell, classToAdd) => {
   cell.classList.add(classToAdd);
 };
 
+// Função para definir a classe CSS do tabuleiro para indicar de quem é a vez
 const setBoardHoverClass = () => {
   board.classList.remove("circle");
   board.classList.remove("x");
@@ -74,35 +98,43 @@ const setBoardHoverClass = () => {
   }
 };
 
+// Função para alternar a vez entre X e Círculo
 const swapTurns = () => {
   isCircleTurn = !isCircleTurn;
 
   setBoardHoverClass();
 };
 
+// Função para lidar com o clique em uma célula
 const handleClick = (e) => {
-  // Colocar a marca (X ou Círculo)
+  // Obtém a célula clicada
   const cell = e.target;
+
+  // Determina o símbolo a ser colocado (X ou Círculo)
   const classToAdd = isCircleTurn ? "circle" : "x";
 
   placeMark(cell, classToAdd);
 
-  // Verificar por vitória
+  // Verifica se houve uma vitória
   const isWin = checkForWin(classToAdd);
 
-  // Verificar por empate
+  // Verifica se o jogo empatou
   const isDraw = checkForDraw();
 
+  // Se houve uma vitória, encerra o jogo
   if (isWin) {
     endGame(false);
   } else if (isDraw) {
+    // Se o jogo empatou, encerra o jogo como empate
     endGame(true);
   } else {
-    // Mudar símbolo
+    // Caso contrário, troca de jogador
     swapTurns();
   }
 };
 
+// Inicia o jogo quando a página é carregada
 startGame();
 
-restartButton.addEventListener("click", startGame);
+// Adiciona um ouvinte de evento para o botão de reiniciar o jogo
+restartButton.addEventListener("click", startGame); 
--- cronometro/script.js
+++ cronometro/script.js
@@ -1,8 +1,9 @@
-// script.js
-let tempoAtual = 0;
-let interval;
-let emExecucao = false;
+// Inicialização de variáveis
+let tempoAtual = 0;  // Armazena o tempo atual em segundos
+let interval;        // Armazena o identificador do intervalo usado para atualizar o tempo
+let emExecucao = false;  // Indica se o cronômetro está em execução
 
+// Função para formatar o tempo no formato HH:MM:SS
 function formatarTempo(tempo) {
     const horas = Math.floor(tempo / 3600);
     const minutos = Math.floor((tempo % 3600) / 60);
@@ -10,26 +11,31 @@ function formatarTempo(tempo) {
     return `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
 }
 
+// Função para atualizar o tempo e exibi-lo na página
 function atualizarTempo() {
-    tempoAtual++;
-    document.querySelector('.tempo').textContent = formatarTempo(tempoAtual);
+    tempoAtual++;  // Incrementa o tempo atual em 1 segundo
+    document.querySelector('.tempo').textContent = formatarTempo(tempoAtual);  // Atualiza a exibição do tempo na página
 }
 
+// Event listener para o botão "Iniciar"
 document.querySelector('.iniciar').addEventListener('click', () => {
-    if (!emExecucao) {
-        interval = setInterval(atualizarTempo, 1000);
-        emExecucao = true;
+    if (!emExecucao) {  // Verifica se o cronômetro não está em execução
+        interval = setInterval(atualizarTempo, 1000);  // Inicia um intervalo de 1 segundo para atualizar o tempo
+        emExecucao = true;  // Define que o cronômetro está em execução
     }
 });
 
+// Event listener para o botão "Parar"
 document.querySelector('.parar').addEventListener('click', () => {
-    clearInterval(interval);
-    emExecucao = false;
+    clearInterval(interval);  // Para o intervalo de atualização do tempo
+    emExecucao = false;  // Define que o cronômetro não está mais em execução
 });
 
+
+// Event listener para o botão "Zerar"
 document.querySelector('.zerar').addEventListener('click', () => {
-    clearInterval(interval);
-    emExecucao = false;
-    tempoAtual = 0;
-    document.querySelector('.tempo').textContent = formatarTempo(tempoAtual);
+    clearInterval(interval);  // Para o intervalo de atualização do tempo
+    emExecucao = false;  // Define que o cronômetro não está mais em execução
+    tempoAtual = 0;  // Reseta o tempo atual para zero
+    document.querySelector('.tempo').textContent = formatarTempo(tempoAtual);  // Atualiza a exibição do tempo na página
 });
